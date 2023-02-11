e371ca51a32fb6bb3d39c4f0d13e0c84a709125c
dehayat <dehayat0@gmail.com>
2023-02-11 17:39:22 +0100

    added game states and player states

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,17 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
+public enum GameState
+{
+    WaitingForPlayersToJoin,
+    WaitingForPlayersToChoose,
+    ResolvingChoices
+}
+
+/// <summary>
+/// This is the main controller and only works on the host
+/// </summary>
 public class GameController : NetworkBehaviour
 {
+    [SerializeField]
+    private GameState currentState;
+    private int playerCount;
+    private int playerChooseCount;
+
+    public override void Spawned()
+    {
+        if (Object.HasStateAuthority)
+        {
+            currentState = GameState.WaitingForPlayersToJoin;
+            playerCount = 0;
+        }
+    }
 
     [Rpc(sources: RpcSources.All, targets: RpcTargets.StateAuthority)]
-    public void RPC_Configure(RpcInfo info = default)
+    public void RPC_PlayerJoin(RpcInfo info = default)
     {
-        Debug.Log(info.Source.PlayerId);
+        playerCount++;
+        if (playerCount == 2)
+        {
+            currentState = GameState.WaitingForPlayersToChoose;
+            playerChooseCount = 0;
+        }
     }
+
     [Rpc(sources: RpcSources.All, targets: RpcTargets.StateAuthority)]
-    public void RPC_Choose(string choice, RpcInfo info = default)
+    public void RPC_Choose(string choice, Player player, RpcInfo info = default)
     {
         Debug.Log(info.Source.PlayerId + " Chose " + choice);
+        player.currentState = PlayerState.chosen;
+        playerChooseCount++;
+        if (playerChooseCount == 2)
+        {
+            currentState = GameState.ResolvingChoices;
+            Debug.Log("Resolving choices");
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
+public enum PlayerState
+{
+    waiting,
+    choosing,
+    chosen
+}
+/// <summary>
+/// This is used on every client but each client does their own shit
+/// </summary>
 public class Player : NetworkBehaviour
 {
+
+    [Networked(OnChanged = nameof(OnStateChanged))] public PlayerState currentState { get; set; } = PlayerState.choosing;
+
+    public static void OnStateChanged(Changed<Player> changed)
+    {
+        changed.Behaviour.OnStateChanged();
+    }
+
+    private void OnStateChanged()
+    {
+        if (currentState == PlayerState.waiting)
+        {
+        }
+        if (currentState == PlayerState.choosing)
+        {
+            //allow choosing
+            //show ui
+        }
+        if (currentState == PlayerState.chosen)
+        {
+            //dont allow choosing
+            //Remove gui i guess
+        }
+    }
+
     public override void Spawned()
     {
         if (Object.HasInputAuthority)
         {
-            FindAnyObjectByType<GameController>().RPC_Configure();
+            FindAnyObjectByType<GameController>().RPC_PlayerJoin();
+            currentState = PlayerState.waiting;
         }
     }
     public override void FixedUpdateNetwork()
@@ -17,16 +52,16 @@ public class Player : NetworkBehaviour
     }
     private void OnGUI()
     {
-        if (Object.HasInputAuthority)
+        if (Object.HasInputAuthority && currentState == PlayerState.choosing)
         {
 
             if (GUI.Button(new Rect(0, 0, 200, 40), "Rock"))
             {
-                FindAnyObjectByType<GameController>().RPC_Choose("Rock");
+                FindAnyObjectByType<GameController>().RPC_Choose("Rock", this);
             }
             if (GUI.Button(new Rect(0, 40, 200, 40), "Paper"))
             {
-                FindAnyObjectByType<GameController>().RPC_Choose("Paper");
+                FindAnyObjectByType<GameController>().RPC_Choose("Paper", this);
             }
         }
     }
